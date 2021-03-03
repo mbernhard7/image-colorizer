91db8c95302f1e0ae3bfcee5741330d02cf4114a
Miles Bernhard <milesbernhard@Miles-MacBook-Pro.local>
2021-03-02 16:29:26 -0800

    Working on returning image

--- image-colorizer.py
+++ image-colorizer.py
@@ -15,7 +15,7 @@ def colorize():
 		file = request.files['imageFile']
 		img = Image.open(file.stream)
 		img_io = BytesIO()
-		pil_img.save(img_io, 'JPEG', quality=70)
+		img.save(img_io, 'JPEG', quality=70)
 		img_io.seek(0)
 		return send_file(img_io, mimetype='image/jpeg'), 200
 
