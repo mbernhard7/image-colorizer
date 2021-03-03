35be7c130dd13415037f0db99df7f5fb345b308e
Miles Bernhard <milesbernhard@Miles-MacBook-Pro.local>
2021-03-02 16:03:51 -0800

    Added tabs

--- static/scripts/tabs.js
+++ static/scripts/tabs.js
@@ -1,23 +1,16 @@
-function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
+function openPage(pageName, elmnt) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.background = "";
   }
-  
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.background = "#1FB264";
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
