808108d0e95730a7f2f64132bc4f6a93fcd23cfc
Miles Bernhard <milesbernhard@Miles-MacBook-Pro.local>
2021-03-15 19:13:38 -0400

    Moved pkl file to Google Cloud Storage

--- image-colorizer.py
+++ image-colorizer.py
@@ -9,33 +9,41 @@ import torch
 import torchvision.transforms as T
 from io import BytesIO
 import base64
-
-app = Flask(__name__)
+import urllib.request
 import torch
+app = Flask(__name__)
+#set to run on CPU
 torch.device("cpu")
+#fetch model from Google Cloud Storage
+body=urllib.request.urlopen("https://storage.googleapis.com/image-colorizer-model/export.pkl").read()
+#load model
+model=load_learner(BytesIO(body), cpu=True)
 
-model=load_learner(open("./models/export.pkl",'rb'), cpu=True)
-
+#serve main page
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('main.html')
 
+#colorize image
 @app.route('/colorize', methods=['POST'])
 def colorize():
 	try:
+		#get file and extension
 		file = request.files['imageFile']
 		extension=file.filename.split('.')[-1]
+		#convert image to RGB and get bytes
 		img = PIL.Image.open(file.stream).convert('RGB')
 		img_io = BytesIO()
 		img.save(img_io, extension.upper(), quality=100)
 		img_io.seek(0)
+		#create fastai image and make prediciton
 		img_fastai = PILImage.create(img_io)
 		prediction,_,probability= model.predict(img_fastai)
 		img=PIL.ImageOps.invert(img)
-		
-		encoded_img = base64.encodebytes(img_io.getvalue()).decode('ascii') # encode as base64
-
+		#encode image as base64 to send back
+		encoded_img = base64.encodebytes(img_io.getvalue()).decode('ascii')
+		#send response
 		data={'image':"data:image/"+extension.lower()+";base64,"+encoded_img, 'prediction':prediction, 'probability':'{:0.6e}'.format(probability[1].item())}
 		return make_response(jsonify(data), 200)
 	except Exception as e:
