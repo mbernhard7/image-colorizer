d76f7bf60bc36cd75fdbe56c19b95fcb9f7e659d
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2021-03-18 05:25:11 -0700

    typo fix

--- image_colorizer.py
+++ image_colorizer.py
@@ -8,21 +8,22 @@ from io import BytesIO
 import base64
 
 # fetch and load model from Google Cloud Storage
-model_url="https://storage.googleapis.com/image-colorizer-model/export.pkl"
+model_url = "https://storage.googleapis.com/image-colorizer-model/export.pkl"
 body = urllib.request.urlopen(model_url).read()
 model = load_learner(BytesIO(body), cpu=True)
 
+
 def colorize_file(file, extension):
-	"""Invert image colors, make prediction using Pets NN
-	
-	Args:
-	    file (file-like object): file taken from post request
-	    extension (string): file ending
-	
-	Returns:
-	    Dictionairy: image data and prediction data
-	"""
-	# convert image to RGB and get bytes
+    """Invert image colors, make prediction using Pets NN
+
+    Args:
+        file (file-like object): file taken from post request
+        extension (string): file ending
+
+    Returns:
+        Dictionairy: image data and prediction data
+    """
+    # convert image to RGB and get bytes
     img = PIL.Image.open(file.stream).convert('RGB')
     img_io = BytesIO()
     img.save(img_io, extension.upper(), quality=100)
