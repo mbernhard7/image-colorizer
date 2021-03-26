cdd1bbca8c8d40d29aefdf9ea91408dd9b40de02
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2021-03-25 18:50:31 -0700

    fixed gitignore

--- deploy-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-cd ~/Documents/Code/CS121/image-colorizer/frontend
-php -S localhost:8001 &
-cd ~/Documents/Code/CS121/image-colorizer/backend
-flask run -h localhost -p 8002
--- deploy-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-cd ~/Documents/Code/CS121/image-colorizer/
-git add .
-git commit -m "deploy"
-git push heroku `git subtree split --prefix frontend $(git rev-parse --abbrev-ref HEAD)`:main --force
