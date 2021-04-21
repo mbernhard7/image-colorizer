9e7c071005cefcacfca3db9532495a344f7bd129
Miles Bernhard <mbernhard22@students.claremontmckenna.edu>
2021-04-21 16:54:14 -0700

    fixed gitignore

--- backend/reinstall_reqs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ENV=ic-backend
-source $WORKON_HOME/$ENV/bin/activate
-pip uninstall -y -r <(pip freeze)
-pip install -r requirements.txt
--- backend/update_backend.py
+++ backend/update_backend.py
@@ -9,6 +9,9 @@ import shutil
 import filecmp
 import zipfile
 
+app_changed=False
+requirements_changed=False
+this_changed=False
 if os.path.exists('backend-new'):
     shutil.rmtree(os.path.abspath('backend-new'))
 os.mkdir('backend-new')
@@ -30,12 +33,26 @@ for directory, subdirectories, files in os.walk('backend-new'):
                 os.remove(old_name)
                 os.rename(new_name,old_name)
                 print("Replaced "+old_name+" with new version.\n")
+                app_changed=True
+                if 'requirements' in new_name:
+                    requirements_changed=True
+                if 'update_backend' in new_name:
+                    this_changed=True
             else:
                 print("No changes detected.\n")
 if os.path.exists('backend-new'):
     shutil.rmtree(os.path.abspath('backend-new'))
     print('Deleted download.\n')
-print(subprocess.run(['bash', 'reinstall_reqs.sh'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-print("\nReloaded virtualenv.")
-Path('/var/www/milesbernhard_pythonanywhere_com_wsgi.py').touch() #triggers web app reload
-print("\nReloaded app.")
+if requirements_changed:
+    print('Requirements changed.')
+    print(subprocess.run(['bash', 'reinstall_reqs.sh'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+    print("\nReloaded virtualenv.")
+if app_changed:
+    print('App changed.')
+    Path('/var/www/milesbernhard_pythonanywhere_com_wsgi.py').touch() #triggers web app reload
+    print("\nReloaded app.")
+    if this_changed:
+        print('\nUpdate script changed. Triggering re-run...')
+        print(subprocess.run(['bash', 'reupdate_backend.sh', str(os.getpid())], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+else:
+    print('No changes detected.\nDone.')
